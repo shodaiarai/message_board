0096596dc2cca428533c9cca73fff514346f5349
admin <admin@192.168.10.108>
2023-01-11 13:55:17 +0900

    EditServlet.javaを作成

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -52,7 +52,7 @@ public class CreateServlet extends HttpServlet {
             em.getTransaction().commit();
             em.close();
 
-            response.sendRedirect(request.getContextPath() + "/index");
+            response.sendRedirect(request.getContextPath() + "/Index");
         }
     }
 
